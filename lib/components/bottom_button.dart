255aaeaeaffd58e19225a58977eb2b329239e49f
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 00:47:21 +0100

    commit bottom button widget

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({@required this.textButton,@required this.onPress});
+
+  final String textButton;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: this.onPress,
+      child: Container(
+        color : kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10,),
+        padding: EdgeInsets.only(bottom: 10),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        child: Center(
+          child: Text(
+            this.textButton,
+            style: kBottomButton,
+          ),
+        ),
+      ),
+    );
+  }
+}
