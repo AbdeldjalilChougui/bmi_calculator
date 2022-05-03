ee0d98e46cba841697242692b209206c3cc02575
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 00:48:17 +0100

    commit card widget

---
+++ lib/components/reusable_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.color,this.cardChild,this.onPress});
+
+  final Color color;
+  final Widget cardChild;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: this.onPress,
+      child: Container (
+        child: this.cardChild,
+        width: 150,
+        height: 160,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: this.color,
+        ),
+      ),
+    );
+  }
+}
+
