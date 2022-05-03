b8d147d951149471f73ef2074e00c368a5bd01cc
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 00:47:46 +0100

    commit consts

---
+++ lib/constants.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+const double kBottomContainerHeight = 60;
+const Color kActiveCardColor = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kActiveCardTextColor = Color(0xFFFFFFFF);
+const Color kInactiveCardTextColor = Color(0xFF8D8E98);
+const Color kBottomContainerColor = Color(0xFFEB1555);
+
+const TextStyle kTextStyle =  TextStyle (
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
+
+const TextStyle kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+
+const TextStyle kBottomButton = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kTitleTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kBMITextStyle = TextStyle(
+  fontSize: 100,
+  fontWeight: FontWeight.w900,
+);
+
+const TextStyle kInterTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kActiveTextStyle =  TextStyle (
+  fontSize: 18,
+  color: Color(0xFFFFFFFF),
+);
+
+const TextStyle kInactiveTextStyle =  TextStyle (
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
